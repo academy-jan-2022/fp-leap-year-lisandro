347e1ad5bc04b9ba4aeebe1312a5fbe69fbb264f
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-17 11:41:30 +0100

    green

--- src/FPFizzBuzz/FPLeapYear.Tests/Tests.fs
+++ src/FPFizzBuzz/FPLeapYear.Tests/Tests.fs
@@ -7,3 +7,13 @@ open Xunit
 let ``is not a leap year when NOT divisible by 4`` (year, expected) =
   let result = LeapYear.check year
   Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData(1997, 4, false)>]
+let ``should return false when the number is not divisible by the divisor`` (number, divisor, expected) =
+  let result =
+    match number with
+    | LeapYear.IsNotDivisibleBy divisor -> false
+    |_ -> true
+    
+  Assert.Equal(expected, result)
